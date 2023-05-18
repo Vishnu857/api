2c52ee98f3bcd3aba54db8d545ce898b383c1031
vishnu857 <vishnuchandran2532@gmail.com>
2023-05-19 03:28:15 +0530

    second

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var cors = require("cors");
 const app = express();
 app.use(cors());
 app.get("/", (req, res) => {
-  res.json({ name: "bala", age: 21 });
+  res.json({ name: "bala", age: 20 });
 });
 app.listen(3000);
