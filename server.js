deb86dfaec535286a3d3e8c4f8a1afeef30dc698
vishnu857 <vishnuchandran2532@gmail.com>
2023-05-22 02:03:25 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var cors = require("cors");
 const app = express();
 app.use(cors());
 app.get("/", (req, res) => {
-  res.json({ name: "bala", age: 20 });
+  res.json([
+    { name: "joker", age: 30 },
+    { name: "ironman", age: 30 },
+    { name: "watchman", age: 30 },
+    { name: "superman", age: 30 },
+    { name: "spider man", age: 29 },
+  ]);
 });
 app.listen(3000);
